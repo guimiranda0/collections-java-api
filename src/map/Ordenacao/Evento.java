90b39538c8c2a903f39bbd95d237af907680aaba
Guilherme <guimsjesus@gmail.com>
2024-07-09 15:08:54 -0300

    feat: Implementa as classes Evento e AgendaEventos

---
+++ src/map/Ordenacao/Evento.java
@@ -0,0 +1,20 @@
+package map.Ordenacao;
+
+public class Evento {
+
+    private String nome;
+    private String atracao;
+
+    public Evento(String nome, String atracao) {
+        this.nome = nome;
+        this.atracao = atracao;
+    }
+
+    public String getAtracao() {
+        return atracao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
